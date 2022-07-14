13887b4e43b66796f898b3f625fe4c494a714a27
chenjunji <247814310@qq.com>
2022-07-14 15:50:37 +0800

    字符串与切片转换

--- strings.go
+++ strings.go
@@ -1,6 +1,9 @@
 package go_utils
 
-import "strings"
+import (
+	"strings"
+	"unsafe"
+)
 
 // TrimAllSpace 去除所有空格类字符
 func TrimAllSpace(str *string) string {
@@ -9,4 +12,14 @@ func TrimAllSpace(str *string) string {
 	s = strings.Replace(s, "\n", "", -1)
 	s = strings.Replace(s, " ", "", -1)
 	return s
+}
+
+func SliceByteToString(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
+}
+
+func StringToSliceByte(s string) []byte {
+	x := (*[2]uintptr)(unsafe.Pointer(&s))
+	h := [3]uintptr{x[0], x[1], x[1]}
+	return *(*[]byte)(unsafe.Pointer(&h))
 }
